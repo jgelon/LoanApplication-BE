e2b342978504c3f7cf532dda9927374319793bf7
Jurn van Mieghem <jurn.van.mieghem@alten.nl>
2021-10-20 12:44:49 +0200

    Code cleanup

--- src/main/java/nl/testautomation/demoapplication/backend/DemoapplicationApplication.java
+++ src/main/java/nl/testautomation/demoapplication/backend/DemoapplicationApplication.java
@@ -1,19 +1,11 @@
 package nl.testautomation.demoapplication.backend;
 
-import lombok.extern.slf4j.Slf4j;
-import nl.testautomation.demoapplication.backend.model.LoanRequest;
-import nl.testautomation.demoapplication.backend.model.LoanType;
-import nl.testautomation.demoapplication.backend.model.LoanReason;
-import nl.testautomation.demoapplication.backend.repository.LoanReasonsRepository;
-import nl.testautomation.demoapplication.backend.repository.LoanRequestRepository;
-import nl.testautomation.demoapplication.backend.repository.LoanTypeRepository;
 import nl.testautomation.demoapplication.backend.service.DemoGeneratorService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@Slf4j
 @SpringBootApplication
 public class DemoapplicationApplication {
 
