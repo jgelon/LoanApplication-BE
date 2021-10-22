e61aa69880d83855b4b6d58d89324576dcd5a931
Jurn van Mieghem <jurn.van.mieghem@alten.nl>
2021-10-22 15:17:14 +0200

    Cleaner code

--- src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
+++ src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
@@ -49,14 +49,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
-                // dont authenticate this particular request
+                // Only authenticate comments everything else is permitted
                 .authorizeRequests().antMatchers("/comments/**").authenticated()
                 .anyRequest().permitAll()
-                .and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
-                    .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
