7847b528eefbdf2fd62566d718efae68d9ab591b
Jurn van Mieghem <j.gelon@gmail.com>
2021-10-26 20:39:46 +0200

    Fix CORS issue

--- src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
+++ src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 // Only authenticate comments everything else is permitted
                 .authorizeRequests().antMatchers("/comments/**").authenticated()
                 .anyRequest().permitAll()
