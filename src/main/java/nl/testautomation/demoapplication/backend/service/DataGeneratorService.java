e939cd49ee5c48184a80c10ab5705e0c07637ebe
Jurn van Mieghem <j.gelon@gmail.com>
2022-05-16 12:22:43 +0200

    Cleanup datagenerator

--- src/main/java/nl/testautomation/demoapplication/backend/service/DataGeneratorService.java
+++ src/main/java/nl/testautomation/demoapplication/backend/service/DataGeneratorService.java
@@ -1,17 +1,13 @@
 package nl.testautomation.demoapplication.backend.service;
 
 import com.github.javafaker.Faker;
-
 import nl.testautomation.demoapplication.backend.enums.Decision;
 import nl.testautomation.demoapplication.backend.enums.Gender;
 import nl.testautomation.demoapplication.backend.enums.IncomeType;
 import nl.testautomation.demoapplication.backend.enums.MaritalStatus;
-import nl.testautomation.demoapplication.backend.model.LoanReason;
 import nl.testautomation.demoapplication.backend.model.LoanRequest;
 import nl.testautomation.demoapplication.backend.model.LoanType;
-import nl.testautomation.demoapplication.backend.repository.LoanReasonsRepository;
 import nl.testautomation.demoapplication.backend.repository.LoanRequestRepository;
-import nl.testautomation.demoapplication.backend.repository.LoanTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,71 +17,17 @@ import java.util.List;
 import java.util.Locale;
 
 @Service
-public class DemoGeneratorService {
+public class DataGeneratorService {
 
     private static final SecureRandom random = new SecureRandom();
     private final Faker faker = new Faker(new Locale("nl"));
 
-    @Autowired
-    LoanTypeRepository loanTypeRepository;
-    @Autowired
-    LoanReasonsRepository loanReasonsRepository;
     @Autowired
     LoanRequestRepository loanRequestRepository;
 
     @Autowired
     LoanTypeService loanTypeService;
 
-    public void generateDefaultLoanTypes() {
-        loanTypeRepository.save(new LoanType()
-                .setTitle("Mini-loan")
-                .setMinAmount(300));
-        loanTypeRepository.save(new LoanType()
-                .setTitle("Personal loan")
-                .setMinAmount(1500));
-        loanTypeRepository.save(new LoanType()
-                .setTitle("Revolving credit")
-                .setMinAmount(2500));
-        loanTypeRepository.save(new LoanType()
-                .setTitle("Car-loan")
-                .setMinAmount(2000));
-        loanTypeRepository.save(new LoanType()
-                .setTitle("Mortgage")
-                .setMinAmount(50000));
-    }
-
-    public void generateDefaultLoanReasons() {
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("Groceries")
-                        .setDescription("It is not a good idea to apply for a loan for just groceries.")
-        );
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("Investments")
-                        .setDescription("Perhaps it is not such a good idea to apply for a loan for just investments.")
-        );
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("Bills")
-                        .setDescription("A Revolving Credit or a Personal loan is probably the way to go.")
-        );
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("Furniture")
-                        .setDescription("A mini-loan or a Personal loan is probably the best choice.")
-        );
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("Car")
-                        .setDescription("A Car-loan is the best choice when buying a car.")
-        );
-        loanReasonsRepository.save(
-                new LoanReason()
-                        .setTitle("House")
-                        .setDescription("You need a mortgage.")
-        );
-    }
 
     public List<LoanRequest> generateLoanRequests(int amount) {
         List<LoanType> allLoanTypes = loanTypeService.getAllLoanTypes();
