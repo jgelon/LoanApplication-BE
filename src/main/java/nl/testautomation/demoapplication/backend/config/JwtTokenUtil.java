e956371ec4488fad824f197cf5f7dbf96a24d8b7
Jurn van Mieghem <jurn.vanmieghem@ad.dfnld.nl>
2022-05-25 14:07:11 +0200

    Fixed Login

--- src/main/java/nl/testautomation/demoapplication/backend/config/JwtTokenUtil.java
+++ src/main/java/nl/testautomation/demoapplication/backend/config/JwtTokenUtil.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Component
-@NoArgsConstructor
 public class JwtTokenUtil implements Serializable {
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60L;
     private static final long serialVersionUID = -2550185165626007488L;
