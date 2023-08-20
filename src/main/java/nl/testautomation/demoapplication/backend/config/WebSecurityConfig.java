84b3a66e2d2ee9230cd32164af4799e23b27b056
Jurn van Mieghem <jurn.vanmieghem@ad.dfnld.nl>
2023-08-20 10:39:01 +0200

    Combine code

--- src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
+++ src/main/java/nl/testautomation/demoapplication/backend/config/WebSecurityConfig.java
@@ -3,16 +3,19 @@ package nl.testautomation.demoapplication.backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import nl.testautomation.demoapplication.backend.service.JwtUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -22,9 +25,11 @@ public class WebSecurityConfig {
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
 
     @Bean
-    public AuthenticationProvider authenticationProvider(UserDetailsService jwtUserDetailsService) {
+    public AuthenticationProvider authenticationProvider() {
         var provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(jwtUserDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
@@ -40,22 +45,21 @@ public class WebSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
             throws Exception {
         return config.getAuthenticationManager();
-            }
+    }
 
     @Bean
-  public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .cors((request) -> request.disable())
-        .csrf(AbstractHttpConfigurer::disable)        
-        .exceptionHandling(r -> r.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-        .sessionManagement(r -> r.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(requests -> requests
-            .requestMatchers("/comments/**", "/loanrequests/admin/**").authenticated()
-            .anyRequest().permitAll()
-        )
-        .authenticationProvider(authenticationProvider())
-        // Add a filter to validate the tokens with every request
-        .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    return httpSecurity.build();
-  }
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(s -> s.disable())
+                .csrf(s -> s.disable())
+                .exceptionHandling(r -> r.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(r -> r.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/comments/**", "/loanrequests/admin/**").authenticated()
+                        .anyRequest().permitAll())
+                .authenticationProvider(authenticationProvider())
+                // Add a filter to validate the tokens with every request
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 }
