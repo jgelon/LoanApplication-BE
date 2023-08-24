abb5766340615638569a3f067f7840c10a8ff1b9
Jurn van Mieghem <jurn.vanmieghem@ad.dfnld.nl>
2023-08-24 22:52:02 +0200

    Fix swagger doc

--- src/main/java/nl/testautomation/demoapplication/backend/config/ApiDocConfig.java
+++ src/main/java/nl/testautomation/demoapplication/backend/config/ApiDocConfig.java
@@ -3,20 +3,11 @@ package nl.testautomation.demoapplication.backend.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiDocConfig {
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("loanapplication-public")
-                .pathsToMatch("/**")
-                .build();
-    }
-
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
